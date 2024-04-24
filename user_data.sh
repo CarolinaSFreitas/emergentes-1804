341217a2e5b313582ac68d459c28b90474a4484e
Carolina Freitas <99994934+CarolinaSFreitas@users.noreply.github.com>
2024-04-24 13:34:10 -0300

    add shellscript

---
+++ user_data.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+echo "====================Updating Ubuntu===================="
+sudo apt update
+
+echo "====================Installing Node.js===================="
+curl -fsSL https://deb.nodesource.com/setup_21.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+echo "====================Node.js version===================="
+node -v
+
+echo "====================Installing NPM===================="
+sudo npm install -g npm@latest
+
+echo "====================NPM version===================="
+npm -v
+
+echo "====================Cloning repository with Next.js project===================="
+git clone https://github.com/CarolinaSFreitas/Testando.git || { echo "Failed to clone repository"; exit 1; }
+
+echo "====================Changing directory===================="
+cd Testando || { echo "Failed to change directory"; exit 1; }
+
+echo "====================Installing dependencies===================="
+npm install || { echo "Failed to install dependencies"; exit 1; }
+
+echo "====================Building Next.js project===================="
+npm run build || { echo "Failed to build project"; exit 1; }
+
+echo "====================Installing PM2===================="
+sudo npm install pm2@latest -g
+
+echo "====================Starting project with PM2===================="
+pm2 start npm --name Testando -- run start -- -p 3000 || { echo "Failed to start project with PM2"; exit 1; }
+
+echo "====================Listing Next.js project with PM2===================="
+pm2 list Testando
+
+echo "Initialization completed successfully"
